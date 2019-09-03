a575cf280177c3ecfd65773903f5024a631e33bf
neorusse <neorusse@gmail.com>
2019-09-03 19:41:01 +0100

    Login route implement

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template, redirect, g
+from flask import Flask, render_template, redirect, jsonify, session, request
+# from flask_session import Session
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from sql import SQL
 
+# Instantiate app
 app = Flask(__name__)
 
 db = SQL("sqlite:///green_leaf.db")
 
-@app.route("/", methods=["GET"])
-def get_something():
-    loc  = db.execute('SELECT * FROM locations')
-    return render_template("index.html", loctn=loc[0]["locations"])
+# root route
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+# login route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  # User reached route via POST (as by submitting a form via POST)
+  if request.method == "POST":
+    # Grab form data
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if email:
+      if password:
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE email = email")
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+          return render_template("/login.html", msg="invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+      else:
+        return render_template("/login.html", msg="Please supply password")
+    else:
+      return render_template("/login.html", msg="Please supply email")
+
+
 
