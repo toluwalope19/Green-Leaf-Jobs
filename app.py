1202b6cf9ee88ba8567ca7b5a8dca5879f53d164
cidospark <cidospark27@gmail.com>
2019-09-04 09:11:17 +0100

    updated ui

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, jsonify, session, request
 # from flask_session import Session
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
+#from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+#from werkzeug.security import check_password_hash, generate_password_hash
 
 from sql import SQL
 
@@ -18,28 +18,29 @@ def index():
 # login route
 @app.route("/login", methods=["GET", "POST"])
 def login():
-  # User reached route via POST (as by submitting a form via POST)
-  if request.method == "POST":
-    # Grab form data
-    email = request.form.get("email")
-    password = request.form.get("password")
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Grab form data
+        email = request.form.get("email")
+        password = request.form.get("password")
 
-    if email:
-      if password:
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE email = email")
+        if email:
+            if password:
+                # Query database for username
+                rows = db.execute("SELECT * FROM users WHERE email = email")
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
-          return render_template("/login.html", msg="invalid username and/or password")
+                # Ensure username exists and password is correct
+                #if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+                    #return render_template("/login.html", msg="invalid username and/or password")
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+                    # Remember which user has logged in
+                    #session["user_id"] = rows[0]["id"]
 
-      else:
-        return render_template("/login.html", msg="Please supply password")
+            else:
+                return render_template("/login.html", msg="Please supply password")
+        else:
+            return render_template("/login.html", msg="Please supply email")
+
     else:
-      return render_template("/login.html", msg="Please supply email")
-
-
+        return render_template("/login.html")
 
