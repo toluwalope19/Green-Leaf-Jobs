af7e5d8f99c47e22ad7741d5ef47b4a360e3a8c5
russell nyorere <neorusse@gmail.com>
2019-09-07 16:46:53 +0100

    Define job, industries, & location functions

--- helper.py
+++ helper.py
@@ -1,8 +1,21 @@
 import os
 from flask import redirect, render_template, request, session
 
-
-
 def loggedIn_user_info(db):
     rows = db.execute("SELECT id, email, password, user_type, photo FROM users WHERE id = :id", id=session["user_id"])
     return rows
+
+# Get Job Functions from db
+def job_function(db):
+  job_query = db.execute("SELECT * FROM job_functions")
+  return job_query
+
+# Get industires from db
+def ind_function(db):
+  ind_query = db.execute("SELECT * FROM industries")
+  return ind_query
+
+# Get locations from db
+def loctn_function(db):
+  loctn_query = db.execute("SELECT * FROM locations")
+  return loctn_query
