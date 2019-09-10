a85247b7649aa2dc7c874ba3ea66018d919d5736
cidospark <cidospark27@gmail.com>
2019-09-10 10:42:09 +0100

    quick fix

--- helper.py
+++ helper.py
@@ -51,8 +51,10 @@ def fetch_jobs(db):
 
 #fetch jobs
 def fetch_search(db, job_id, ind_id, loc_id):
-    srch_rslt = db.execute("SELECT * FROM vacancies INNER JOIN job_functions ON vacancies.job_func_id = job_functions.id"+
-                           " WHERE job_func_id=:jID", jID=job_id)
+    srch_rslt = db.execute("SELECT * FROM users INNER JOIN company "+
+        "ON users.id=company.user_id INNER JOIN vacancies ON vacancies.user_id=company.id WHERE job_func_id=:jID ORDER BY id DESC", jID=job_id)
+    # db.execute("SELECT * FROM vacancies INNER JOIN job_functions ON vacancies.job_func_id = job_functions.id"+
+    #                        " WHERE job_func_id=:jID", jID=job_id)
     return srch_rslt
 
 
